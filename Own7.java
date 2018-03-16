b8c1e5212e2b8bdad2e4c9e44cab39df941d44a4
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-03-16 17:11:01 +0530

    Create Own7.java

---
+++ Own7.java
@@ -0,0 +1,32 @@
+public class Own7
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int c[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            c[i]=0;
+        }
+        for(int i=0;i<n;i++)
+        {
+           for(int j=0;j<n;j++)
+           {   
+               if(arr[i]==arr[j])
+               {
+                  c[i]=c[i]+1;
+               }
+           }
+        }
+        for(int i=0;i<c.length;i++)
+        {
+            if(c[i]<2)
+            {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
